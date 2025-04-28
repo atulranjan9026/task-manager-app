a9292474ffaf658190105bffbcfdaaf484d04f58
Atul <99653239+atulranjan9026@users.noreply.github.com>
2025-04-28 18:56:19 +0530

    cors

--- server/src/index.js
+++ server/src/index.js
@@ -12,7 +12,7 @@ const app = express();
 // Middleware
 app.use(cors({
   origin: process.env.NODE_ENV === 'production' 
-    ? 'https://task-manager-web.onrender.com' 
+    ? 'https://task-manager-app-8wkx.onrender.com' 
     : 'http://localhost:3000'
 }));
 app.use(express.json());
